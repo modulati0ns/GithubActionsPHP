c30518455c61e13d2a3d4224afd51aa5b5255a1b
Javier Martínez <javimtmz96@gmail.com>
2021-12-18 20:20:30 +0100

    Uso correcto de namespaces

--- tests/SumaTest.php
+++ tests/SumaTest.php
@@ -1,8 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
-include('src/Suma.php');
+use src\Suma;
 
 class SumaTest extends TestCase
 {
@@ -11,5 +10,11 @@ class SumaTest extends TestCase
     {
         $suma = new Suma(1, 2);
         $this->assertEquals(3, $suma->sumar(1, 2));
+<<<<<<< Updated upstream
+=======
+
+        // Test que falla
+        // $this->assertFalse(4, $suma->sumar(1, 2));
+>>>>>>> Stashed changes
     }
 }
